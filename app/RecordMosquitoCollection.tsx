60a299d528bf4881ee046c6d27bae58f6aac659b
Turner Hayes <dev@turnerhayes.com>
2024-07-22 15:41:05 +0200

    Add image preview to collection form

--- app/RecordMosquitoCollection.tsx
+++ app/RecordMosquitoCollection.tsx
@@ -25,10 +25,48 @@ const getDateString = (date: Date) => {
     }`;
 };
 
+const PhotoDisplay = (
+    {
+        photoUrl,
+    }: {
+        photoUrl: string;
+    }
+) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const toggleCollapsed = useCallback(() => {
+        setIsCollapsed(!isCollapsed);
+    }, [
+        isCollapsed,
+        setIsCollapsed,
+    ]);
+
+    return (
+        <div className="px-3">
+            <header>
+                <button
+                    onClick={toggleCollapsed}
+                >
+                    {
+                        isCollapsed ?
+                            "Show image" :
+                            "Hide Image"
+                    }
+                </button>
+            </header>
+            <img
+                src={photoUrl}
+                className={`${isCollapsed ? "hidden" : ""}`}
+            />
+        </div>
+    );
+};
+
 export const RecordMosquitoCollection = () => {
     const [collectionDate, setCollectionDate] = useState<Date|null>(new Date());
     const [numMosquitoes, setNumMosquitoes] = useState<number|null>(null);
     const [photoId, setPhotoId] = useState<PhotoId|null>(null);
+    const [photoUrl, setPhotoUrl] = useState<string|null>(null);
 
     const dispatch = useAppDispatch();
 
@@ -64,13 +102,20 @@ export const RecordMosquitoCollection = () => {
     const handlePhotoChange = useCallback(async (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (!file) {
+            if (photoUrl) {
+                URL.revokeObjectURL(photoUrl);
+                setPhotoUrl(null);
+            }
             return;
         }
 
         const photoId = await savePhoto(file);
         setPhotoId(photoId);
+        setPhotoUrl(URL.createObjectURL(file));
     }, [
         setPhotoId,
+        setPhotoUrl,
+        photoUrl,
     ]);
 
     return (
@@ -84,7 +129,7 @@ export const RecordMosquitoCollection = () => {
                 <p className="mb-8 mt-6 text-xl">
                     Enter information about your captured mosquitoes here.
                 </p>
-                <div className="flex-1 flex flex-col items-start justify-center">
+                <div className="flex-1 flex flex-col items-start justify-center w-4/5">
                     <label className="flex flex-col items-start m-4">
                         <div className="mr-3">
                             Date:
@@ -124,6 +169,13 @@ export const RecordMosquitoCollection = () => {
                             onChange={handlePhotoChange}
                         />
                     </label>
+                    {
+                        photoUrl ? (
+                            <PhotoDisplay
+                                photoUrl={photoUrl}
+                            />
+                        ) : null
+                    }
 
                     <button
                         type="submit"
