704000642e3b4ec898de68fa618e3905f116d2c1
Turner Hayes <dev@turnerhayes.com>
2024-07-24 09:11:29 +0200

    Remove some config options for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = isProd ? ({
     basePath: isProd ? "/mosquito_hunter" : undefined,
-    output: "export",
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
 }) : {};
 
 export default nextConfig;
