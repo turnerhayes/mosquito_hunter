3d976eab784a0c9896b1218df621cea3a7e3cbb2
Turner Hayes <dev@turnerhayes.com>
2024-07-24 09:13:19 +0200

    Remove unused ternary

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = isProd ? ({
-    basePath: isProd ? "/mosquito_hunter" : undefined,
+    basePath: "/mosquito_hunter",
 }) : {};
 
 export default nextConfig;
