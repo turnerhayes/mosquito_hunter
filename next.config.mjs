f9e8b1669bf35787f4a3cfe9c5313eb8581fb945
Turner Hayes <dev@turnerhayes.com>
2024-07-24 09:32:18 +0200

    clear next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-let nextConfig = {};
-
-if (isProd) {
-    nextConfig = ({
-        basePath: "/mosquito_hunter",
-    });
-}
+const nextConfig = {};
 
 export default nextConfig;
