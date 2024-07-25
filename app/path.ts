92b817ec6840f5acfed2a8246f3f35e7a1acc42f
Turner Hayes <dev@turnerhayes.com>
2024-07-25 15:14:13 +0200

    Make path regex global to prevent client error

--- app/path.ts
+++ app/path.ts
@@ -1,1 +1,1 @@
-export const BASE_PATH = (process.env["NEXT_PUBLIC_BASE_PATH"]?.replaceAll(/^\s+|\/?\s$/, "") || "");
+export const BASE_PATH = (process.env["NEXT_PUBLIC_BASE_PATH"]?.replaceAll(/^\s+|\/?\s$/g, "") || "");
