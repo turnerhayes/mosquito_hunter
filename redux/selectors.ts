1fafbd8097fee7411031ef9db46bb0fd2aa75143
Turner Hayes <dev@turnerhayes.com>
2024-08-06 20:00:41 -0400

    Removed unused selectors and util

--- redux/selectors.ts
+++ redux/selectors.ts
@@ -2,22 +2,13 @@ import { RootState } from "@/redux/store";
 import { createSelector } from "@reduxjs/toolkit";
 import { SettingName } from "./slices/settings_slice";
 
-export const getBreedingSites = (state: RootState) =>
-  state.breedingSites.sites;
-
-export const getMosquitoTraps = (state: RootState) =>
-  state.mosquitoTraps.traps;
-
-export const getCollectionRecords = (state: RootState) =>
-  state.collectionRecords.records;
-
 export const getSettings = (state: RootState) =>
   state.settings;
 
 export const getSetting = createSelector(
   [
     getSettings,
-    (state: RootState, settingName: SettingName) => settingName,
+    (_state: RootState, settingName: SettingName) => settingName,
   ],
   (settings, settingName) => settings[settingName]
 );
